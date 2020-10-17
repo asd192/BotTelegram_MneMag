bc882c9f297bd40dcd9da3121a18d3b8351f53c5
Dmitry <PanshinDA@yandex.ru>
2020-10-18 02:04:10 +0500

    Добавлены новые функции. Добавлен запрос url-shops.

---
+++ bot_main.py
@@ -0,0 +1,95 @@
+import telebot
+from telebot import types
+from db_queries import link_shops_read
+
+# добываем токен
+with open("API-Token.txt", "r", encoding="UTF8") as token:
+    token = token.readlines()[2].strip()
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def menu_down(message):
+    """крафт и отправка нижнего меню"""
+    keyboard_down = types.ReplyKeyboardMarkup(True, False)
+
+    keyboard_down.row('Магазины')
+    keyboard_down.row('На сайт')
+    keyboard_down.row('Помощь')
+
+    bot.send_message(message.chat.id, "Привет. Выбирай.", reply_markup=keyboard_down)
+
+
+@bot.message_handler(content_types=['text'])
+def get_menu_shops(message):
+    """крафт и отправка чат-меню"""
+    if message.text == 'Магазины':
+
+        shops_dict = {
+            'Детские': 'Детские',
+            'Дом\Сад\Дача': 'Для дома и сада',
+            'Косметика\Здоровье': 'Косметика и здоровье',
+            'Игры\Софт': 'Игры \ Софт',
+            'Одежда\Обувь': 'Одежда \ Обувь',
+            'Книги': 'Книги',
+            'Спорт\Активный отдых': 'Спорт \ Активный отдых',
+            'Обучение': 'Обучение',
+            'Ювелирные': 'Ювелирные',
+            'Строительство\Ремонт': 'Строительство \ Ремонт',
+            'Техника\Электроника': 'Техника \ Электроника',
+            'Транспорт\Запчасти': 'Транспорт \ Запчасти',
+            'Подарки\Сувениры': 'Подарки \ Сувениры',
+            'Искусство': 'Искусство',
+            'Животные\Растения': 'Животные\Растения',
+            'Мегамаркеты': 'МегаМаркеты'
+        }
+
+        # выбираем тип клавиатуры
+        keyboard_shops = types.InlineKeyboardMarkup()
+
+        # крафтим кнопоки
+        shop_list, columns, = [], 3
+        for name_button, name_db in shops_dict.items():
+            line_buttons = types.InlineKeyboardButton(text=name_button, callback_data=name_db)
+            shop_list.append(line_buttons)
+            if len(shop_list) == columns:
+                keyboard_shops.add(shop_list[0], shop_list[1], shop_list[2])
+                shop_list.clear()
+        if len(shop_list) == 2:
+            keyboard_shops.add(shop_list[0], shop_list[1])
+        else:
+            keyboard_shops.add(shop_list[0])
+
+        # отправляем кнопки пользователю
+        bot.send_message(message.from_user.id, text='Выбирай категорию', reply_markup=keyboard_shops)
+
+    if message.text == 'На сайт':
+        # выбираем тип клавиатуры
+        keyboard_site = types.InlineKeyboardMarkup()
+
+        # крафтим кнопоку
+        key_url_main = types.InlineKeyboardButton(text='MneMag.ru', url='https://mnemag.ru')
+        keyboard_site.add(key_url_main)
+
+        # отправляем кнопку пользователю
+        bot.send_message(message.chat.id, "Нажми на кнопку для перехода на сайт", reply_markup=keyboard_site)
+
+    if message.text == 'Помощь':
+        bot.send_message(message.chat.id, "Магазины - меню магазинов\nНа сайт - ссылка на сайт\nПомощь - помощь")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    """Если сообщение поступило из чата с ботом, запросить ссылки"""
+    if call.message:
+        shop_urls = link_shops_read(call.data)
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text=shop_urls)
+
+    # Если сообщение из инлайн-режима
+    # elif call.inline_message_id:
+    #     print('inline message')
+
+
+bot.polling(none_stop=True, interval=0)
---
+++ db_queries.py
@@ -0,0 +1,55 @@
+﻿import datetime
+import pymysql
+
+
+def log_errors(message_error):
+    # TODO не работает
+    with open("db_queries.log", "a", encoding="UTF8") as dbq_log:
+        dbq_log.write(f'{datetime.datetime.now()} - {message_error}\n')
+
+
+try:
+    with open("database_access.txt", "r", encoding="UTF8") as dt_acc:
+        param_db = [val.strip() for val in dt_acc]
+
+    connect = pymysql.connect(
+        host=param_db[0],
+        db=param_db[1],
+        user=param_db[2],
+        password=param_db[3],
+        charset=param_db[4],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    log_errors("Успешное подключение к БД\n")
+except:
+    log_errors("Не удалось подключиться к БД\n")
+
+
+def link_shops_read(call_data='Мегамаркеты'):
+    """запрос ссылок магазинов из БД"""
+    try:
+        with connect.cursor() as cursor:
+            sql_shops = """
+            SELECT
+                SUBSTRING_INDEX(ccs.s_url, '|', 1) url
+            FROM cms_con_shop ccs
+            JOIN cms_con_shop_cats ccsc ON ccsc.id = ccs.category_id
+            WHERE
+                ccsc.title = (%s) AND
+                is_hidden IS null AND
+                date_approved IS NOT null AND
+                is_pub = 1 AND
+                is_deleted IS null AND
+                is_private = 0
+            ORDER BY RAND()
+            LIMIT 5
+            """
+            cursor.execute(sql_shops, call_data)
+        url = [row['url'] for row in cursor]
+        return url
+    except:
+        log_errors("Неудачный запрос ссылок\n")
+
+
+if __name__ == '__main__':
+    print(link_shops_read())
